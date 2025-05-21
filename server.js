16de365df4591fb04914dad52ef9822d5819269c
neo <rasdacorentin@gmail.com>
2025-05-22 00:40:55 +0400

    ça vibe un peu trop mdr...

--- server.js
+++ server.js
@@ -1,13 +1,24 @@
 const express = require('express');
 const app = express();
+const fs = require('fs');
 const port = 3000;
 
 app.use(express.static('public'));
 app.use(express.json());
 
-app.post('/api/donnees', (req, res) => {
-	console.log('Données reçues :', req.body);
-	res.json({ message: 'Reçu !' });
+app.post('/api/chemin', (req, res) => {
+
+	const { aile, salle } = req.body;
+	console.log(aile);
+	console.log(salle);
+
+	/*fs.readdir(path, (err, files) => {
+		if (err) return res.status(404).json([]);
+		const imgPaths = files
+			.filter(f => f.endsWith('.jpg'))
+			.map(f => `/images/${aile}/${salle}/${f}`);
+		res.json(imgPaths);
+	});*/
 });
 
 app.listen(port, () => {
