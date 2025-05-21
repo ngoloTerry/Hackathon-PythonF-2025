d52304e558c2ca948f5e2e04aace79a5d99bebe3
ngoloTerry <nourdine.tahiry@gmail.com>
2025-05-21 23:05:00 +0400

    page index.html sans tailwind

--- extracteur.js
+++ extracteur.js
@@ -1,7 +1,18 @@
 /* Il faut pouvoir récupérer l'URL du fichier .ics à exploiter */
+let fichier = fetch('gunga_ginga.csv')
+      .then(response => response.text())
+      .then(csvText => {
+        document.getElementById('csvText').textContent = csvText;
+      })
+      .catch(error => {
+        document.getElementById('csvText').textContent = 'Erreur de chargement du fichier CSV.';
+        console.error(error);
+      });
+      let contenu = document.getElementById("contenu");
+      contenu.textContent = fichier;
 
-/* Une fois fait, il nous faut transformer en donnée exploitable dans un tableau les infos suivantes*/
-
+      
+/* Une fois fait, il nous faut transformer en donnée exploitable dans un tableau .csv les infos suivantes*/
 /*Le cours en question 
 Date
 Plage Horraire 
@@ -11,10 +22,6 @@ Groupe
 Salle de classe */
 
 // Donner, par rapport à l'heure actuelle, le prochain cours et le chemin vers celui-ci
-
-
-
-
 
 // fichiers des photos
 // base de données 
