561c8a88775b433917c8b95d39b6389545662c8b
ngoloTerry <nourdine.tahiry@gmail.com>
2025-05-22 00:24:50 +0400

    overlay recherche sur menu principal

--- public/libpublic/script.js
+++ public/libpublic/script.js
@@ -1,17 +1,22 @@
 $(document).ready(function () {
-	$('#envoyer').click(function () {
-		$.ajax({
-			url: '/api/donnees',
-			method: 'POST',
-			contentType: 'application/json',
-			data: JSON.stringify({ info: 'Données envoyées avec jQuery' }),
-			success: function (data) {
-				$('#reponse').text(data.message);
-			},
-			error: function () {
-				$('#reponse').text('Erreur lors de l’envoi.');
-			}
-		});
-	});
-});
+    const $est = $("#est");
+    const $ouest = $("#ouest");
+    const $titre = $("#titre");
+    const $recherche = $("#recherche");
 
+    window.click_est = function () {
+        $est.addClass("w-full").removeClass("flex-1");
+        $ouest.addClass("hidden");
+        $titre.addClass("hidden");
+
+        $recherche.fadeIn(500); // ou slideDown()
+    };
+
+    window.click_ouest = function () {
+        $ouest.addClass("w-full").removeClass("flex-1");
+        $est.addClass("hidden");
+        $titre.addClass("hidden");
+
+        $recherche.fadeIn(500);
+    };
+});
