3a50d81944ce62d3b18b4b57a339cf1c301a0bb2
neo <rasdacorentin@gmail.com>
2025-05-23 02:15:01 +0400

    tri des images mise en place de l'algo d'affichage du resultat

--- public/libpublic/script.js
+++ public/libpublic/script.js
@@ -1,32 +1,52 @@
 $(function () {
     //Gestion recherche 
-    const salles = ["BU Sciences", "Bibliotheque", "UFR ST", "UFR Science", "Amphi Charpak", "Amphi Commerson", "Amphi D", "Amphi C", "S1-...", "S2-...", "S3-...", "S4a-...", "S4b-...", "S4d-...", "S4c-..."];
+    const salles = [
+        {salle : "BU Sciences", num : 1}, 
+        {salle : "Bibliotheque U" , num : 1}, 
+        {salle : "UFR ST", num : 2}, 
+        {salle : "UFR Science", num : 2}, 
+        {salle : "Amphi Charpak", num : 3}, 
+        {salle : "Amphi Commerson", num : 4}, 
+        {salle : "Amphi B", num : 5}, 
+        {salle : "Amphi A", num : 6}, 
+        {salle : "Amphi C", num : 7}, 
+        {salle : "S1-...", num : 8}, 
+        {salle : "S2-...", num : 9}, 
+        {salle : "S3-...", num : 10}, 
+        {salle : "S4a-...", num : 11}, 
+        {salle : "S4b-...", num : 12}, 
+        {salle : "S4d-...", num : 13}, 
+        {salle : "S4c-...", num : 14}
+    ];
 
     $('#search').on('input', function () {
         let value = $(this).val().toLowerCase();
         let $suggestions = $('#suggestions');
         $suggestions.empty();
 
-        let matches = salles.filter(salle => salle.toLowerCase().includes(value));
+        let matches = salles.filter(salleO => salleO.salle.toLowerCase().includes(value));
 
         if (value && matches.length) {
             $suggestions.removeClass('hidden');
             matches.forEach(match => {
                 let $li = $('<li></li>')
-                    .text(match)
-                    .addClass('p-2 hover:bg-indigo-100 cursor-pointer')
+                    .text(match.salle)
+                    .data('num', match.num)
+                    .addClass('p-num :  hover:bg-indigo-100 cursor-pointer')
 
                     .on('click', function () {
-                        let match = $(this).text();
-                        $('#search').val(match);
+                        let salle = $(this).text();
+                        let num = $(this).data('num');
+                        $('#search').val(salle);
                         $suggestions.addClass('hidden');
 
                         // Envoie le choix et la salle au serveur
-                        let direction = $("#est").hasClass("w-full") ? "est" : "ouest";
+                        let isWest = $("#est").hasClass("w-full") ? 0 : 1;
 
                         $.post('/api/choix', {
-                            direction: direction,
-                            salle: match
+
+                            direction: isWest, //Est = 0 ; West = 1
+                            salle: num
                         }, function () {
                             window.location.href = '/resultat'; 
                         });
@@ -37,6 +57,7 @@ $(function () {
             $suggestions.addClass('hidden');
         }
     });
+
     // Gestion avant-page
     let est = $("#est");
     let ouest = $("#ouest");
@@ -50,8 +71,7 @@ $(function () {
         titre.addClass("hidden");
 
         recherche
-
-            .addClass("flex")            // active Flexbox .hide()                      // reset pour fadeIn
+            .addClass("flex")
             .fadeIn(500);                // animation propre
     };
 
--- server.js
+++ server.js
@@ -1,8 +1,63 @@
 const express = require('express');
 const app = express();
-const fs = require('fs');
 const port = 3000;
 
+const sallesW = [
+	{num: 0, suite : 14, exceptions : ["Centre", "Centre2", "S4c"], description : "au Batiment S4C"}
+	,{num : 1, suite :6, exceptions : null, description : "à la BU Sciences" }
+	,{num : 2, suite :7 , exceptions : null, description : "à l'UFR Sciences"}
+	,{num : 3, suite : 7, exceptions : null, description : "à l'Amphi Charpak"}
+	,{num : 4, suite : 14, exceptions : ["Centre","Centre2"], description : "à l'Amphi Commerson"}
+	,{num : 5, suite : 14, exceptions : ["Centre","Centre2","S4d"], description : "à l'Amphi B"}
+	,{num : 6,  suite : 14, exceptions : ["Centre","Centre2","S4d"], description : "à l'Amphi A"}
+	,{num : 7,  suite : 14, exceptions : ["Centre","Centre2","S4c"], description : "à l'Amphi C"}
+	,{num : 8,  suite : 7, exceptions : null, description : "au Batiment S1"}
+	,{num : 9,  suite : 10, exceptions : null, description : "au Batiment S2"}
+	,{num : 10,  suite : 14, exceptions : ["Centre","Centre2"], description : "au Batiment S3"}
+	,{num : 11,  suite : 14, exceptions : ["Centre","Centre2", "S4a"], description : "au Batiment S4A"}
+	,{num : 12,  suite : 11, exceptions : ["S4b"], description : "au Batiment S4B"}
+	,{num : 13,  suite : 13, exceptions : ["Centre","Centre2", "S4d"], description : "au Batument S4D"}
+]
+const sallesE = [
+	{num: 0, suite : 10, exceptions : ["Centre", "Centre2", "S4c"], description : "au Batiment S4C"}
+	,{num : 1, suite : 17, exceptions : null, description : "à la BU Sciences" }
+	,{num : 2,  suite : 16, exceptions : null, description : "à l'UFR Sciences"}
+	,{num : 3,  suite : 16, exceptions : null, description : "à l'Amphi Charpak"}
+	,{num : 4,  suite : 10, exceptions : ["Centre", "Centre2"], description : "à l'Amphi Commerson"}
+	,{num : 5,  suite : 10, exceptions : ["Centre","Centre2","S4d"], description : "à l'Amphi B"}
+	,{num : 6,  suite : 10, exceptions : ["Centre","Centre2","S4d"], description : "à l'Amphi A"}
+	,{num : 7,  suite : 10, exceptions : ["Centre","Centre2","S4c"], description : "à l'Amphi C"}
+	,{num : 8,  suite : 16, exceptions : null, description : "au Batiment S1"}
+	,{num : 9,  suite : 11, exceptions : null, description : "au Batiment S2"}
+	,{num : 10,  suite : 10, exceptions : ["Centre", "Centre2"], description : "au Batiment S3"}
+	,{num : 11,  suite : 10, exceptions : ["Centre","Centre2","S4a"], description : "au Batiment S4A"}
+	,{num : 12,  suite : 10, exceptions : ["S4b"], description : "au Batiment S4B"}
+	,{num : 13,  suite : 10, exceptions : ["Centre","Centre2","S4d"], description : "au Batument S4D"}
+]
+
+/*
+ const salles = [
+	{salle : "BU Sciences", num : 1}, 
+	{salle : "Bibliotheque U" , num : 1}, 
+	{salle : "UFR ST", num : 2}, 
+	{salle : "UFR Science", num : 2}, 
+	{salle : "Amphi Charpak", num : 3}, 
+	{salle : "Amphi Commerson", num : 4}, 
+	{salle : "Amphi B", num : 5}, 
+	{salle : "Amphi A", num : 6}, 
+	{salle : "Amphi C", num : 7}, 
+	{salle : "S1-...", num : 8}, 
+	{salle : "S2-...", num : 9}, 
+	{salle : "S3-...", num : 10}, 
+	{salle : "S4a-...", num : 11}, 
+	{salle : "S4b-...", num : 12}, 
+	{salle : "S4d-...", num : 13}, 
+	{salle : "S4c-...", num : 0}
+    ];
+    */
+// E || W -> Destination  
+// E_0 
+// W_0
 let dernierChoix = null;
 
 app.use(express.urlencoded({ extended: true }));
@@ -11,35 +66,24 @@ app.use(express.json());
 
 app.post('/api/choix', (req, res) => {
 	const { direction, salle } = req.body;
+	console.log(salle);
 	dernierChoix = {direction, salle};
 	res.sendStatus(200);
-	/*fs.readdir(path, (err, files) => {
-		if (err) return res.status(404).json([]);
-		const imgPaths = files
-			.filter(f => f.endsWith('.jpg'))
-			.map(f => `/images/${aile}/${salle}/${f}`);
-		res.json(imgPaths);
-	});*/
 });
 app.get('/resultat', (req, res) => {
-	console.log("tldr");
-	res.sendFile(__dirname + '/public/resultat.html'); // selon ton arborescence
+	res.sendFile(__dirname + '/public/resultat.html');
 });
 
 app.get('/api/images', (req, res) => {
 	if (!dernierChoix) return res.status(400).json([]);
-
 	const { direction, salle } = dernierChoix;
 	let images = [];
 
-	if (direction === 'est') {
+	if (!direction) {
+		//
 		images.push('./img/E_1.jpg', './img/E_2.jpg');
-	} else if (direction === 'ouest') {
+	} else {
 		images.push('./img/W_1.jpg', './img/W_2.jpg');
-	}
-
-	if (salle.toLowerCase().includes('charpak')) {
-		images.push('./img/W_6.jpg');
 	}
 
 	res.json(images);
@@ -48,4 +92,5 @@ app.get('/api/images', (req, res) => {
 app.listen(port, () => {
 	console.log(`Serveur lancé sur http://localhost:${port}`);
 });
+// Cette fonction renvoie le nombre d'image à afficher en fonction du numéro en paramètre [9]
 
